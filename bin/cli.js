d46ccbe06e937c1ce8e4366e44f0f77e98826179
Samnang Chhun <samnang.chhun@gmail.com>
2022-10-24 18:30:09 +0700

    Setup build

--- bin/cli.js
+++ bin/cli.js
@@ -2,28 +2,27 @@
 
 import { program } from "commander";
 
-import { ActionCableClient } from "../index.js";
+import { SomlengClient } from "../index.js";
 import SMPPGateway from "../lib/smpp_gateway.js";
 
-program
-  .requiredOption("-k, --key <value>", "Device key")
-  .requiredOption("--smpp-host <value>", "SMPP host")
-  .requiredOption("--smpp-port <value>", "SMPP port", "2775")
-  .requiredOption("--smpp-system-id <value>", "SMPP System ID")
-  .requiredOption("--smpp-password <value>", "SMPP password")
-  .requiredOption("-d, --domain <value>", "Somleng Domain", "ws://localhost:3000")
-  .option("-v, --verbose", "Output extra debugging")
-  .showHelpAfterError()
-  .parse();
-
-const options = program.opts();
-
 async function run() {
-  const actionCableClient = new ActionCableClient({
+  program
+    .requiredOption("-k, --key <value>", "Device key")
+    .requiredOption("--smpp-host <value>", "SMPP host")
+    .requiredOption("--smpp-port <value>", "SMPP port", "2775")
+    .requiredOption("--smpp-system-id <value>", "SMPP System ID")
+    .requiredOption("--smpp-password <value>", "SMPP password")
+    .requiredOption("-d, --domain <value>", "Somleng Domain", "ws://localhost:3000")
+    .option("-v, --verbose", "Output extra debugging")
+    .showHelpAfterError()
+    .parse();
+  const options = program.opts();
+
+  const client = new SomlengClient({
     domain: options.domain,
     deviceKey: options.key,
   });
-  await actionCableClient.subscribe();
+  await client.subscribe();
 
   const smppGateway = new SMPPGateway({
     host: options.smppHost,
@@ -34,13 +33,13 @@ async function run() {
   });
   await smppGateway.connect();
 
-  actionCableClient.onNewMessage(async (message) => {
+  client.onNewMessage(async (message) => {
     const deliveryReceipt = await smppGateway.sendMessage({
       destination: message.to,
       short_message: message.message,
     });
 
-    await actionCableClient.notifyDeliveryReceipt({
+    await client.notifyDeliveryReceipt({
       id: message.id,
       externalMessageId: deliveryReceipt.messageId,
       status: deliveryReceipt.status,
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import ActionCableClient from "./lib/action_cable_client.js";
+import SomlengClient from "./lib/somleng_client";
 
-export { ActionCableClient };
+export { SomlengClient };
