f3e9e99ba678973bd520ecc355a3e2702d6a70f7
Samnang Chhun <samnang.chhun@gmail.com>
2022-11-08 15:37:40 +0700

    WIP

--- bin/cli.js
+++ bin/cli.js
@@ -1,62 +1,75 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
-import { readFileSync } from "fs";
+import { program, Option } from "commander";
 
 import SomlengClient from "../lib/somleng_client.js";
-import SMPPGateway from "../lib/smpp_gateway.js";
+import { GoIPGateway, DummyGateway } from "../lib/gateways/index.js";
 
 async function main() {
+  let options = {};
+  let gateway;
+
   program
-    // .requiredOption("-k, --key <value>", "Device key")
-    // .addOption(new Option("-m, --model <value>").choices(["goip"]))
-    // .requiredOption("--goip-smpp-host <value>", "SMPP host")
-    // .requiredOption("--goip-smpp-port <value>", "SMPP port", "2775")
-    // .requiredOption("--goip-smpp-system-id <value>", "SMPP System ID")
-    // .requiredOption("--goip-smpp-password <value>", "SMPP password")
-    .requiredOption("-c, --credentials <file>", "Credentials file")
+    .requiredOption("-k, --key <value>", "Device key")
     .requiredOption("-d, --domain <value>", "Somleng Domain", "wss://app.somleng.org")
     .option("-v, --verbose", "Output extra debugging")
-    .showHelpAfterError()
-    .parse();
-  const options = program.opts();
+    .showHelpAfterError();
 
-  const configData = readFileSync(options.credentials);
-  const config = JSON.parse(configData);
+  program
+    .command("goip")
+    .description("connect to GoIP Gateway")
+    .requiredOption("--goip-smpp-host <value>", "SMPP host")
+    .requiredOption("--goip-smpp-port <value>", "SMPP port", "2775")
+    .requiredOption("--goip-smpp-system-id <value>", "SMPP System ID")
+    .requiredOption("--goip-smpp-password <value>", "SMPP password")
+    .requiredOption("--goip-channels <value>", "Number of channels")
+    .action((commandOptions) => {
+      gateway = new GoIPGateway({
+        host: commandOptions.goipSmppHost,
+        port: commandOptions.goipSmppPort,
+        systemId: commandOptions.goipSmppSystemId,
+        password: commandOptions.goipSmppPassword,
+        channels: commandOptions.goipChannels,
+        debug: program.opts().verbose,
+      });
+    });
 
-  config.verbose = options.verbose || config.verbose;
-  config.domain = options.domain || config.domain;
+  program
+    .command("dummy")
+    .description("connect to dummy Gateway")
+    .requiredOption("--dummy-host <value>", "Dummy host")
+    .action((commandOptions) => {
+      gateway = new DummyGateway({
+        host: commandOptions.dummyHost,
+      });
+    });
+
+  program.parse();
+  options = { ...options, ...program.opts() };
+
+  await gateway.connect();
 
   const client = new SomlengClient({
-    domain: config.domain,
-    deviceKey: config.key,
+    domain: options.domain,
+    deviceKey: options.key,
   });
   await client.subscribe();
 
-  const smppGateways = {};
-  config.channels.forEach(async (channel) => {
-    smppGateways[channel.id] = new SMPPGateway({
-      host: channel.smppHost,
-      port: channel.smppPort,
-      systemId: channel.systemId,
-      password: channel.password,
-      debug: config.verbose,
-    });
-
-    await smppGateways[channel.id].connect();
-  });
-
   client.onNewMessage(async (message) => {
-    const deliveryReceipt = await smppGateways[message.channelId].sendMessage({
-      destination: message.to,
-      short_message: message.message,
-    });
+    try {
+      const deliveryReceipt = await gateway.sendMessage(message.channelId, {
+        destination: message.to,
+        short_message: message.message,
+      });
 
-    await client.notifyDeliveryReceipt({
-      id: message.id,
-      externalMessageId: deliveryReceipt.messageId,
-      status: deliveryReceipt.status,
-    });
+      await client.notifyDeliveryReceipt({
+        id: message.id,
+        externalMessageId: deliveryReceipt.messageId,
+        status: deliveryReceipt.status,
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
   });
 }
 
