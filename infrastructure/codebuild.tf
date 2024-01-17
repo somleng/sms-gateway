7f599f3c967455d15f838571c6879c90c3fa35e2
David Wilkie <dwilkie@gmail.com>
2024-01-17 16:33:21 +0700

    WIP

--- infrastructure/codebuild.tf
+++ infrastructure/codebuild.tf
@@ -2,6 +2,10 @@ locals {
   codebuild_identifier = "sms-gateway"
 }
 
+data "aws_s3_bucket" "artifacts" {
+  bucket = "ci-artifacts.somleng.org"
+}
+
 data "aws_iam_policy_document" "codebuild_assume_role" {
   statement {
     effect = "Allow"
@@ -58,7 +62,10 @@ resource "aws_codebuild_project" "linux_arm64" {
   service_role = aws_iam_role.codebuild.arn
 
   artifacts {
-    type = "NO_ARTIFACTS"
+    type = "S3"
+    location = data.aws_s3_bucket.artifacts.bucket
+    packaging = "NONE"
+    namespace_type = "BUILD_ID"
   }
 
   environment {
