ba77c4deaefa0447fedca5c18cf806633b8208f4
David Wilkie <dwilkie@gmail.com>
2024-01-17 15:37:24 +0700

    WIP

--- infrastructure/codebuild.tf
+++ infrastructure/codebuild.tf
@@ -52,34 +52,7 @@ resource "aws_iam_role_policy_attachment" "codebuild_ecr" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
 
-resource "aws_codebuild_project" "amd64" {
-  name           = "${local.codebuild_identifier}-linux-amd64"
-
-  service_role = aws_iam_role.codebuild.arn
-
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  cache {
-    type  = "LOCAL"
-    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
-  }
-
-  environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    image = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
-    type = "LINUX_CONTAINER"
-    privileged_mode = true
-  }
-
-  source {
-    type = "GITHUB"
-    location = "https://github.com/somleng/sms-gateway.git"
-  }
-}
-
-resource "aws_codebuild_project" "arm64" {
+resource "aws_codebuild_project" "linux_arm64" {
   name           = "${local.codebuild_identifier}-linux-arm64"
 
   service_role = aws_iam_role.codebuild.arn
@@ -88,16 +61,10 @@ resource "aws_codebuild_project" "arm64" {
     type = "NO_ARTIFACTS"
   }
 
-  cache {
-    type  = "LOCAL"
-    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
-  }
-
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
     image = "aws/codebuild/amazonlinux2-aarch64-standard:3.0"
     type = "ARM_CONTAINER"
-    privileged_mode = true
   }
 
   source {
