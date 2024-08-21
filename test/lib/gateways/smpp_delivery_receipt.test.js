3d3844a3f75109421822f127afeb5980390b5efd
David Wilkie <dwilkie@gmail.com>
2024-08-21 15:54:11 +0700

    Handle rejected delivery receipt

--- test/lib/gateways/smpp_delivery_receipt.test.js
+++ test/lib/gateways/smpp_delivery_receipt.test.js
@@ -35,6 +35,15 @@ describe(SMPPDeliveryReceipt, () => {
     expect(deliveryReceipt.status).toEqual("failed");
   });
 
+  it("parses rejected receipt from Kannel", () => {
+    const message =
+      "id:577e505a-dbd7-46ab-a04c-5a927ffb85b1 sub:001 dlvrd:000 submit date:2408201338 done date:240820093823 stat:REJECTD err:00B text:You";
+
+    const deliveryReceipt = SMPPDeliveryReceipt.parse(message);
+
+    expect(deliveryReceipt.status).toEqual("failed");
+  });
+
   it("handles unsupported message format", () => {
     const message =
       "id:88316820 sub:0 dlvrd:0 submit date:2302281324 done date:2302281325 err:500 Text:Hello World from out";
